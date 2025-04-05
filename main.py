cf1728a3aaa67e0541e75f4a270f637930f00ef5
ZroXorZ <zjn5650@gmail.com>
2025-04-04 22:30:38 -0600

    B: Framerate

--- main.py
+++ main.py
@@ -4,16 +4,21 @@ from constants import *
 def main():
     pygame.init()
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True: # Game Loop
-        
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
     
 if __name__ == "__main__":
     main()
